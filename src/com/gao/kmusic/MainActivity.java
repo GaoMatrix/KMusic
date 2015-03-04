3a7b8dd512159d49e8c5ce96f803bbfa3677e7c6
GaoMatrix <gcquan08@gmail.com>
2015-03-04 22:48:00 +0800

    Use thread pool to load local songs

--- src/com/gao/kmusic/MainActivity.java
+++ src/com/gao/kmusic/MainActivity.java
@@ -2,15 +2,17 @@
 package com.gao.kmusic;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ListView;
 
 import com.gao.kmusic.adapter.MySongListAdapter;
 import com.gao.kmusic.util.MediaUtil;
+import com.gao.kmusic.util.PromptManager;
 
 public class MainActivity extends Activity {
-    private ListView songListView;
-    private MySongListAdapter songAdapter;
+    private ListView mSongListView;
+    private MySongListAdapter mSongAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +27,36 @@ public class MainActivity extends Activity {
 
     private void loadSongList() {
         MediaUtil.getInstacen().initMusics(getApplicationContext());// 在手机的多媒体数据库中查询声音
-        songAdapter = new MySongListAdapter(getApplicationContext());
-        songListView = (ListView) findViewById(R.id.play_list);
-        songListView.setAdapter(songAdapter);
+        mSongAdapter = new MySongListAdapter(getApplicationContext());
+        mSongListView = (ListView) findViewById(R.id.play_list);
+        mSongListView.setAdapter(mSongAdapter);
+
+        // new InitDataTask().execute();//线程池，如操作的线程过多，等待情况
+        new InitDataTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);// 不用等待
     }
 
+    /**
+     * 音乐资源过多
+     */
+    class InitDataTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            PromptManager.showProgressDialog(MainActivity.this);
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            // 加载多媒体信息
+            MediaUtil.getInstacen().initMusics(MainActivity.this);
+            // SystemClock.sleep(100);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            PromptManager.closeProgressDialog();
+            mSongAdapter.notifyDataSetChanged();
+        }
+    }
 }
